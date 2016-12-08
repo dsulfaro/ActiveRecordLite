52bf247740cea28c3815a54e44ec12b2ac2449fd
Dan Sulfaro <dsulfaro1@gmail.com>
2016-12-07 21:41:10 -0800

    finish attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -2,8 +2,14 @@ require 'byebug'
 
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    names.each do |name|
 
+    names.each do |name|
+      define_method(name) { instance_variable_get("@#{name}") }
     end
+
+    names.each do |name|
+      define_method(name.to_s + "=") { |val| instance_variable_set("@#{name}", val)  }
+    end
+
   end
 end
